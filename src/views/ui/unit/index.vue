53719c50d4a70aef5589f6928a1cde7d29eb2986
huaqingluo <huaqingluo@emotibot.com>
2020-08-26 23:09:45 +0800

    xxxx

--- src/views/ui/unit/index.vue
+++ src/views/ui/unit/index.vue
@@ -43,7 +43,6 @@
                                 </div>
                                 <div v-show="selectedRoom.edit == false">
                                     <sui-button basic color="blue" @click="enableUpdateUnit" v-show="selectedRoom.edit == false">
-                                        {{selectedRoom.edit}}
                                         修改
                                     </sui-button>
                                 </div>
