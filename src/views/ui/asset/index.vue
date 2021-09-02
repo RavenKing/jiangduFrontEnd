64a61e06a72c34eb90fdbf8f892d244f4ec1cdb3
YouHao <youhao0809@163.com>
2021-09-02 14:34:19 +0800

    Update index.vue

--- src/views/ui/asset/index.vue
+++ src/views/ui/asset/index.vue
@@ -708,6 +708,11 @@ export default {
 </script>
 
 <style>
+.el-tag--dark {
+    background-color: #4BA6E6;
+    border-color: #6AABDD;
+    color: #fff;
+}
 .tableTag {
     margin-left: 5px;
     margin-bottom: 1%;
